84693079789c9960f6b9f83d55b94d6002425539
Muhammad Fikri Hadian <programmerfikri@gmail.com>
2019-06-02 16:30:26 +0700

    Relationshop

--- app/Lapangan.php
+++ app/Lapangan.php
@@ -9,4 +9,7 @@ class Lapangan extends Model
     protected $fillable = [
         'id', 'id_mitra', 'nama', 'foto', 'tarif', 'tipe_lapangan', 'created_at',
     ];
+    public function mitra(){
+    	return $this->belongsTo('App\Profile_Mitra','id_mitra');
+    }
 }
--- app/Profile_Customer.php
+++ app/Profile_Customer.php
@@ -9,4 +9,8 @@ class Profile_Customer extends Model
     protected $fillable = [
         'id', 'id_user', 'nama', 'nomor_telephone', 'foto', 'kota', 'alamat', 'no_rek', 'na_rek', 'created_at',
     ];
+
+    public function users(){
+        return $this->belongsTo('App\User','id_user','id');
+    }
 }
