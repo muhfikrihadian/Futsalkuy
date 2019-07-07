b5e9e9e6b69d167e0bae03bb3313604b77405909
BariqDharmawan <bariq.2nd.rodriguez@gmail.com>
2019-07-07 21:18:37 +0700

    [UPDATE] navbar mobile (still bug)

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,4 +1,4 @@
-<nav class="d-none d-md-block navbar navbar-expand-lg navbar-light" id="navDesktop">
+<nav id="navDesktop">
   <ul>
     <li>
       <a href="" class="brand-logo">
@@ -8,17 +8,17 @@
     <li>
       <form action="" class="row justify-content-center" method="get">
         {{ csrf_field() }}
-        <div class="col-3">
+        <div class="col-12 col-md-3 mb-4 mb-md-0 px-0 px-md-2">
           <select required>
             <option value="" selected disabled>Cari Berdasarkan</option>
             <option value="">Kategori Olahraga</option>
             <option value="">Kota</option>
           </select>
         </div>
-        <div class="col">
+        <div class="col-12 col-md-9 mb-4 mb-md-0 px-0 px-md-2">
           <input type="text" placeholder="Cari Lapangan. Contoh: Futsal Cawang" required>
+          <button type="submit" class="btn"><i class='bx bx-search-alt'></i></button>
         </div>
-        <button type="submit" class="btn"><i class='bx bx-search-alt'></i></button>
       </form>
     </li>
     @guest
